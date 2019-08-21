5d86a8d32f8a885e2e7c40fd91924ee75d26cf4d
saeed <saeid.kamyabi71@gmail.com>
2019-08-21 13:26:11 +0430

    implement Error() in error model

--- error.go
+++ error.go
@@ -6,6 +6,10 @@ type Error struct {
 	Message string `json:"message,omitempty"`
 }
 
+func (e Error) Error() string  {
+	return e.Message
+}
+
 func HandleError(err error) error  {
 	return Error{
 		Message: err.Error(),
