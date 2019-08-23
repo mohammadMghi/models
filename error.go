08f52cd726855c7560ef1d4d0b10fddc89d450fa
saeed <saeid.kamyabi71@gmail.com>
2019-08-23 10:18:49 +0430

    improve error handle status code

--- error.go
+++ error.go
@@ -1,17 +1,44 @@
 package models
 
+import (
+	"fmt"
+)
+
 type Error struct {
 	error
 
+	Status  int    `json:"-"`
 	Message string `json:"message,omitempty"`
 }
 
-func (e Error) Error() string  {
+const (
+	NOT_FOUND = 404
+)
+
+func (e Error) Error() string {
 	return e.Message
 }
 
-func HandleError(err error) error  {
+func HandleError(err error) error {
 	return Error{
 		Message: err.Error(),
+	}
+}
+
+func GetError(status int, err ...error) error {
+	var msg string
+	if err != nil && len(err) > 0 {
+		msg = err[0].Error()
+	} else {
+		switch status {
+		case NOT_FOUND:
+			msg = "not found"
+		default:
+			msg = fmt.Sprintf("invalid request, code %d", status)
+		}
+	}
+	return Error{
+		Status:  status,
+		Message: msg,
 	}
 }
