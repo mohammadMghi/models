36cba5b6804b0b94351221b9f870f66645b60257
saeed <saeid.kamyabi71@gmail.com>
2019-08-24 15:58:33 +0430

    add more exceptions

--- error.go
+++ error.go
@@ -13,8 +13,11 @@ type Error struct {
 }
 
 const (
+	BadRequestError   = 404
 	NotFoundError     = 404
-	UnauthorizedError = 403
+	UnauthorizedError = 401
+	ForbiddenError    = 403
+	InternalError     = 500
 )
 
 func (e Error) Error() string {
@@ -49,7 +52,29 @@ func GetError(status int, err ...error) error {
 
 func GetUnAuthorizedError(err ...error) error {
 	if err == nil || len(err) == 0 {
-		err = []error{errors.New("access denied to access this section")}
+		err = []error{errors.New("You are not authorized to access this section")}
 	}
 	return GetError(UnauthorizedError, err...)
+}
+
+func GetForbiddenError(err ...error) error {
+	if err == nil || len(err) == 0 {
+		err = []error{errors.New("Access to this section is denied")}
+	}
+	return GetError(ForbiddenError, err...)
+}
+
+// GetValidationError returns error associated with HTTP Vlidation error
+func GetValidationError(err ...error) error {
+	if err == nil || len(err) == 0 {
+		err = []error{errors.New("Your request is not valid")}
+	}
+	return GetError(BadRequestError, err...)
+}
+
+func GetInternalServiceError(err ...error) error {
+	if err == nil || len(err) == 0 {
+		err = []error{errors.New("Unknown error")}
+	}
+	return GetError(InternalError, err...)
 }
