6313f72c2de3d61018e6928f94627d3773c89543
saeed <saeid.kamyabi71@gmail.com>
2019-09-15 12:54:28 +0430

    has_next in pagination result

--- pagination.go
+++ pagination.go
@@ -7,10 +7,11 @@ type PaginationInfo struct {
 	PageCount  uint64 `json:"pages,omitempty"`
 	Page       uint64 `json:"page,omitempty"`
 	PerPage    uint64 `json:"per_page,omitempty"`
+	HasNext    bool   `json:"has_next,omitempty"`
 }
 
 type PaginateResult struct {
 	ReflectItems reflect.Value  `json:"-"`
-	Items        interface{}  `json:"result,omitempty"`
+	Items        interface{}    `json:"result,omitempty"`
 	Pagination   PaginationInfo `json:"pagination,omitempty"`
 }
