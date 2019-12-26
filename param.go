f71072cbd61a3261d7d882d31b8f2a1a169a52c4
saeed <saeid.kamyabi71@gmail.com>
2019-12-26 19:16:37 +0330

    fix param

--- param.go
+++ param.go
@@ -11,7 +11,7 @@ type Params struct {
 	ParamArray
 }
 
-func (ps Params) Get(name string) (string, bool) {
+func (ps *Params) Get(name string) (string, bool) {
 	for _, entry := range ps.ParamArray {
 		if entry.Key == name {
 			return entry.Value, true
@@ -20,12 +20,15 @@ func (ps Params) Get(name string) (string, bool) {
 	return "", false
 }
 
-func (ps Params) ByName(name string) (va string) {
+func (ps *Params) ByName(name string) (va string) {
 	va, _ = ps.Get(name)
 	return
 }
 
-func (ps Params) Set(param *Param) {
+func (ps *Params) Set(param *Param) {
+	if ps.ParamArray == nil {
+		ps.ParamArray = make([]Param, 0)
+	}
 	for _, entry := range ps.ParamArray {
 		if entry.Key == param.Key {
 			entry.Value = param.Value
